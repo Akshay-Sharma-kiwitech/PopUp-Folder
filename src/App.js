f0a19539610f3bf413cf93eadf992707f5eaba12
Akshay-Sharma-kiwitech <98027922+Akshay-Sharma-kiwitech@users.noreply.github.com>
2022-03-30 11:20:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Folder from './Pages/Folder';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/folder/:id' element={<Folder />} />
+    </Routes>
+  );
+}
+
+export default App;
 
